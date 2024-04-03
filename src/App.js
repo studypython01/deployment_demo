ef709d6173fade674cb8cbf3ae118b1957806375
moisesstudy <moisesstudytech@gmail.com>
2024-04-03 17:08:10 -0400

    changes el baile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Deployment is happening here
+        BREAKING CHANGES - A BAILAR TODO EL MUNDO
       </header>
     </div>
   );
